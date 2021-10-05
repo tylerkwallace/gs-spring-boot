9782401a92fca8a216f01cfb0f263c16e7ae8fc9
JoseJCanedo <canedo_jose@columbusstate.edu>
2021-10-05 21:23:31 +0000

    xkcd spec

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,6 +3,7 @@ package com.example.springboot;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
 
 @Controller
@@ -24,9 +25,20 @@ public class HelloController {
 		return "xkcd";
 	}
 
+	// Rquest parameter method
 	@GetMapping("/xkcdSpec")
-	public String xkcdSpecific(@RequestParam(name="id", required=true) String id, Model model) {
+	public String xkcdSpecific(@RequestParam(name="id", required=false) String id, Model model) {
+		if(id == null){
+			id = "200";
+		}
 		model.addAttribute("id", id);
 		return "xkcdSpecific";
 	}
+
+	// path variable method
+	// @GetMapping("/xkcdSpec/{id}")
+	// public String xkcdSpecific(@PathVariable String id, Model model) {
+	// 	model.addAttribute("id", id);
+	// 	return "xkcdSpecific";
+	// }
 }
